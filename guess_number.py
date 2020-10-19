61e34e9588b6afbd01a0fcc8411482f23dc8bd28
Iris Chen <jin2006613@gmail.com>
2020-10-19 13:44:58 +0800

    basic version

---
+++ guess_number.py
@@ -0,0 +1,13 @@
+# 測驗紙
+import random
+r = random.randint(1,100)
+while True:
+	num = input('請猜數字：')
+	num = int(num)
+	if num == r:
+		print('恭喜猜對了')
+		break
+	elif num > r:
+		print('答案比', num, '小')
+	elif num < r:
+		print('答案比', num, '大')
